9852ba171938d4c190147a63adda4ab1d985acc9
sirahhhhh <meteorscramble@gmail.com>
2017-05-01 14:24:01 +0900

    コメント追加その3

--- Assets/Scripts/DolphinController.cs
+++ Assets/Scripts/DolphinController.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 
 public class DolphinController : MonoBehaviour {
 
+    // イルカの最大、最小加速度
     const float MAX_ACCEL_X = 0.15f;
     const float MAX_ACCEL_Y = 0.15f;
     const float MIN_ACCEL_X = -MAX_ACCEL_X;
     const float MIN_ACCEL_Y = -MAX_ACCEL_Y;
 
+    // 加速度
     const float ADD_ACCEL_X = 0.01f;
     const float ADD_ACCEL_Y = 0.01f;
+
+    // 減衰値
     const float ATTENUATION_X = 0.005f;
     const float ATTENUATION_Y = 0.005f;
 
-    public int MaxHitPoint;
-    public int MaxAttackPower;
+    // Inspectorから設定
+    public int MaxHitPoint;     // 最大HP
+    public int MaxAttackPower;  // 最大攻撃力
 
     public GameObject sprite;
 
     GameObject gameControllerObj;
     GameController gameCtrl;
     Animator DolphinAnimator;
-    AudioSource hitSE;
+    AudioSource hitSE;          // 攻撃時SE
 
+    // 移動制限値
     public float moveMinX, moveMaxX;
     public float moveMinY, moveMaxY;
-    public float MaxDamageTime;
+
+    public float MaxDamageTime;     // ダメージ時間の最大時間(設定用)
 
     float accelX, accelY;
     float DamageTime;
     int HitPoint;
     int AttackPower;
-    bool isAlive;
-    bool isDamage;
+    bool isAlive;       // 生存フラグ
+    bool isDamage;      // ダメージ時間中か
 
     void Awake()
     {
@@ -67,6 +74,7 @@ public class DolphinController : MonoBehaviour {
             DamageTime -= Time.deltaTime;
             if(DamageTime <= 0.0f)
             {
+                // ダメージ時間終了
                 DamageTime = MaxDamageTime;
                 isDamage = false;
                 DolphinAnimator.SetBool("IsDamage", false);
@@ -83,14 +91,12 @@ public class DolphinController : MonoBehaviour {
         // 横方向
         if (Input.GetKey("left"))
         {
-            //moveX = -MOVE_X_VALUE;
             accelX += -ADD_ACCEL_X;
             if (accelX < MIN_ACCEL_X) accelX = MIN_ACCEL_X;
             DolphinAnimator.SetBool("IsLeft", true);
         }
         else if (Input.GetKey("right"))
         {
-            //moveX = MOVE_X_VALUE;
             accelX += ADD_ACCEL_X;
             if (accelX > MAX_ACCEL_X) accelX = MAX_ACCEL_X;
             DolphinAnimator.SetBool("IsLeft", false);
@@ -113,13 +119,11 @@ public class DolphinController : MonoBehaviour {
         // 縦方向
         if (Input.GetKey("up"))
         {
-            //moveY = MOVE_Y_VALUE;
             accelY += ADD_ACCEL_Y;
             if (accelY > MAX_ACCEL_Y) accelY = MAX_ACCEL_Y;
         }
         else if (Input.GetKey("down"))
         {
-            //moveY = -MOVE_Y_VALUE;
             accelY += -ADD_ACCEL_Y;
             if (accelY < MIN_ACCEL_Y) accelY = MIN_ACCEL_Y;
         }
@@ -138,6 +142,7 @@ public class DolphinController : MonoBehaviour {
             }
         }
 
+        //*********************************************
         // 移動範囲制限
         float setX = transform.position.x;
         float setY = transform.position.y;
@@ -145,15 +150,17 @@ public class DolphinController : MonoBehaviour {
         setX += accelX;
         setY += accelY;
 
-        if (setX < moveMinX) setX = moveMinX;
-        if (setX > moveMaxX) setX = moveMaxX;
-        if (setY < moveMinY) setY = moveMinY;
-        if (setY > moveMaxY) setY = moveMaxY;
+        if (setX < moveMinX)        setX = moveMinX;
+        else if (setX > moveMaxX)   setX = moveMaxX;
+        if (setY < moveMinY)        setY = moveMinY;
+        else if (setY > moveMaxY)   setY = moveMaxY;
+        //*********************************************
 
         // 移動
         transform.position = new Vector3(setX, setY, 0.0f);
     }
 
+    // 攻撃時の判定にしか使ってません
     void OnCollisionStay2D(Collision2D collision)
     {
         //　敵の攻撃なら処理しない
@@ -177,6 +184,7 @@ public class DolphinController : MonoBehaviour {
         }
     }
 
+    // ダメージ時(攻撃を受けたとき)の判定にしか使ってません
     void OnCollisionEnter2D(Collision2D collision)
     {
         // ダメージ時間中なら処理しない
@@ -192,6 +200,8 @@ public class DolphinController : MonoBehaviour {
             HitPoint -= 1;
             isDamage = true;
             DolphinAnimator.SetBool("IsDamage", true);
+
+            // HPなくなったら
             if (HitPoint <= 0)
             {
                 isAlive = false;
@@ -201,11 +211,13 @@ public class DolphinController : MonoBehaviour {
         }
     }
 
+    // 生存フラグ取得
     public bool GetIsAlive()
     {
         return isAlive;
     }
 
+    // HP取得
     public int GetHP()
     {
         return HitPoint;
