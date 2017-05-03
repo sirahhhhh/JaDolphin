0704908bef0b52d87f45a8a7829222c0c2f9f1c5
sirahhhhh <meteorscramble@gmail.com>
2017-05-03 17:14:37 +0900

    ボート振り向き処理追加など
    ・ボートの行動(振り向き)処理追加
    ・ボートの左右、ダメージ時リソース、アニメ追加
    ・直下resourcesフォルダにリソース追加と一部リネーム
    ・Unity側のボートresourceを左右でフォルダ分け
    ・銛の存在時間を二倍に
    ・当たり判定(イルカ、ボート)を修正
    　->y方向に大きいように見えたため

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -4,44 +4,91 @@ using UnityEngine;
 
 public class BoatController : MonoBehaviour {
 
+    enum eBOAT_ACT
+    {
+        IDLE,                      // なにもしない
+        CHANGE_DIRECTION_LEFT,     // 左を向く
+        CHANGE_DIRECTION_RIGHT,    // 右を向く
+//        SHOT_SPEAR,                // 銛を撃つ
+        MAX_ACT,
+    }
+
     // 銛発射のためのObj
     public GameObject boatSpear;
     BoatSpear boatSpearScript;
 
-    public float MaxDamageTime;
+    public float MaxDamageTime;         // ダメージ時間の最大時間
+    public float MaxAttackIntervalTime; // 攻撃間隔の最大時間
+    public float MaxActIntervalTime;    // 行動間隔の最大時間
     public int MaxHP;
 
-    float DamageTime;
+    float DamageTime;           // ダメージ時間
+    float attackIntervalTime;   // 攻撃間隔の時間
+    float actIntervalTime;      // 行動間隔の時間
     int HitPoint;
     bool isDead;
     bool isDamage;      // ダメージ時間中か
     bool isAttack;
     bool isLeft;        // 左向いてるか
+    bool isActed;       // 行動時間中か
 
     Animator anime;
 
 	// Use this for initialization
 	void Start () {
+        // コピー用銛をDeactiveに
+        boatSpear.SetActive(false);
+
         DamageTime  = MaxDamageTime;
         HitPoint    = MaxHP;
+        attackIntervalTime = MaxAttackIntervalTime;
+        actIntervalTime = MaxActIntervalTime;
+
         isDead      = false;
         isDamage    = false;
         isAttack    = false;
         isLeft      = false;
+        isActed     = false;
 
         anime = GetComponent<Animator>();
-        boatSpearScript = boatSpear.GetComponent<BoatSpear>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        // 攻撃中なら攻撃間隔時間を減らして0以下になれば
+        // 次の攻撃が出来る
+        if (isAttack)
+        {
+            attackIntervalTime -= Time.deltaTime;
+            if (attackIntervalTime <= 0.0f)
+            {
+                attackIntervalTime = MaxAttackIntervalTime;
+                isAttack = false;
+            }
+        }
+
+        // 行動後時間中待機処理
+        if (isActed)
+        {
+            actIntervalTime -= Time.deltaTime;
+            if(actIntervalTime <= 0.0f)
+            {
+                actIntervalTime = MaxActIntervalTime;
+                isActed = false;
+            }
+        }
+        else
+        {
+            // 行動後時間中でなければ行動
+            Act();
+        }
+
         // ダメージ時間減衰
         // ダメージ受けて一定時間は次のダメージを受けない
         if (isDamage)
         {
-            float DamageDecTime = 0.1f;
-            DamageTime -= DamageDecTime;
-            if (DamageTime < 0.0f)
+            DamageTime -= Time.deltaTime;
+            if (DamageTime <= 0.0f)
             {
                 DamageTime = 0.0f;
                 isDamage = false;
@@ -61,8 +108,59 @@ public class BoatController : MonoBehaviour {
         // 攻撃中なら中断
         if (isAttack) return;
 
-        boatSpearScript.ShotSpear();
+        // 右向きの場合、コピー元の銛と同じの座標に生成
+        float createPosX = boatSpear.gameObject.transform.position.x;
+        float createPosY = boatSpear.gameObject.transform.position.y;
+
+        // 左向きの場合、コピー元の銛から少し左にずらす
+        float adjustPosX = -1.0f;
+        if (isLeft) { createPosX += adjustPosX; }
+
+        GameObject createObj = (GameObject)Instantiate(
+            boatSpear,
+            new Vector3(createPosX,
+                        createPosY,
+                        0.0f),
+            Quaternion.identity);
+        createObj.transform.parent = this.gameObject.transform; // 生成した銛の親を生成元ボートに設定
+
+        boatSpearScript = createObj.GetComponent<BoatSpear>();
+        boatSpearScript.ShotSpear(isLeft);
         isAttack = true;
+    }
+
+    // ボート行動
+    void Act()
+    {
+        // ダメージ時間中でも行動するか要検討
+
+        // 0～3のどれかをランダムに引く
+        int ran = (int)(Random.value * 10 % (int)eBOAT_ACT.MAX_ACT);
+        switch(ran)
+        {
+            // 何もしない、待機
+            case (int)eBOAT_ACT.IDLE:
+                break;
+
+            // 左に向きを変える
+            case (int)eBOAT_ACT.CHANGE_DIRECTION_LEFT:
+                isLeft = true;
+                anime.SetBool("IsLeft", isLeft);
+                break;
+
+            // 右に向きを変える
+            case (int)eBOAT_ACT.CHANGE_DIRECTION_RIGHT:
+                isLeft = false;
+                anime.SetBool("IsLeft", isLeft);
+                break;
+
+            //// 銛を撃つ
+            //case (int)eBOAT_ACT.SHOT_SPEAR:
+            //    // ダメージ時間中でなければ攻撃する
+            //    SpearAttack();
+            //    break;
+        }
+        isActed = true;
     }
 
     // ボートダメージ処理
