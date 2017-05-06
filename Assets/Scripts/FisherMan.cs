f152214f4f6c6249893e202b5f03babce857803e
Mr. Kenmo <mr.kenmokun@gmail.com>
2017-05-06 14:43:44 +0900

    漁師をランダムに選択

--- Assets/Scripts/FisherMan.cs
+++ Assets/Scripts/FisherMan.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FisherMan : MonoBehaviour {
+	// 挿し替えようSpriteの定義
+	public Sprite fisherman1;
+	public Sprite fisherman2;
+	public Sprite fisherman3;
+
+	// 漁船のSpriteRenderer反転用
+	SpriteRenderer fmRender;
 
 	// Use this for initialization
 	void Start () {
-		
+		fmRender = 	this.GetComponent<SpriteRenderer> ();
+		fmRender.sprite = SelectFisherManType ();
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private Sprite SelectFisherManType(){
+		int ran = Random.Range(1, 4);
+		switch(ran)
+		{
+		case 1:
+			return fisherman1;
+
+		case 2:
+			return fisherman2;
+
+		case 3:
+			return fisherman3;
+		}
+		return fisherman1;
 	}
 }
